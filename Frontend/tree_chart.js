7a1b96ca9220bf55d887f621bf2f1bc3bf6710bf
alinajibi1234 <eaa24anaj@students.eaaa.dk>
2025-05-13 12:08:28 +0200

    treemap ændring

--- Frontend/tree_chart.js
+++ Frontend/tree_chart.js
@@ -1,164 +1,151 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
-// Sæt dimensionerne og margenerne for grafen
-var margin = {top: 10, right: 10, bottom: 10, left: 10},
-  width = 1350 - margin.left - margin.right,
-  height = 775 - margin.top - margin.bottom;
 
-// Tilføj en knap til at skifte data
-var buttonContainer = d3.select("#treemap").append("div").attr("class", "button-container");
-buttonContainer.append("button")
-  .attr("id", "toggleData")
-  .text("Vis Eksport")
-  .style("display","block") //centrere knappen
-  .style("margin","0 auto");
+// Sætter dimensioner og margener for grafen
+var margin = {top: 10, right: 20, bottom: 10, left: 20},
+  width = 2000 - margin.left - margin.right,
+  height = 750 - margin.top - margin.bottom;
 
-// Opret en SVG-container
+// Tilføjer SVG-objektet til siden
 var svg = d3.select("#treemap")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
-  .style("display", "block")
-  .style("margin", "0 auto")
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  // Tooltip
+// Tooltip - Definerer tooltip-elementet
 var tooltip = d3.select("body")
   .append("div")
-  .attr("class", "tooltip") //giver en class
+  .attr("class", "tooltip")
   .style("position", "absolute")
   .style("background", "white")
   .style("color", "black")
   .style("padding", "5px")
-  .style("border-radius", "3px")
+  .style("border-radius", "3px");
 
+// Læser data fra CSV-filer
+Promise.all([
+  d3.csv('/DB/treemap_import.csv'),
+  d3.csv('/DB/treemap_export.csv')
+]).then(function([importData, exportData]) {
 
-var currentDataFile = '/DB/treemap_import.csv';
-var colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+  // Behandler importdata
+  importData.forEach(function(d) {
+    d.parent = "root";
+    d.value = +d.INDHOLD;
+    d.type = "Import";
+    d.name = "Import";
+  });
 
-// Funktion til at opdatere treemappet baseret på CSV-fil
-function updateTreemap(dataFile) {
-   // Indlæs CSV-filen
-  d3.csv(dataFile).then(function(data) {
+  // Behandler eksportdata
+  exportData.forEach(function(d) {
+    d.parent = "root";
+    d.value = +d.INDHOLD;
+    d.type = "Export";
+    d.name = "Eksport";
+  });
 
-    // Sørg for at INDHOLD er numerisk
-    data.forEach(d => {
-      d.parent = "root";
-      d.INDHOLD = +d.INDHOLD;
-    });
+  // Samler data i én datastruktur
+  var data = [...importData, ...exportData];
 
-    // Tilføj rodnode
-    data.push({ LAND: "root", parent: null, INDHOLD: 0 });
+  // Tilføjer rodnode
+  data.push({ name: "root", parent: null, value: 0 });
 
-    var root = d3.stratify()
-      .id(function(d) { return d.LAND; })
-      .parentId(function(d) { return d.parent; })
-      (data);
+  // Finder maksimumværdi til farveskalering
+  var maxValue = d3.max(data, function(d) { return d.value; });
 
-    // Beregn størrelse for hver node
-    root.sum(function(d) { return d.INDHOLD; });
+  // Definerer farveskala for import
+  var importColor = d3.scaleLinear()
+    .domain([0, maxValue])
+    .range(["#d4e6f1", "#2e86c1"]);
 
-    // Anvend treemap layout
-    d3.treemap()
-      .size([width, height]) // Sæt størrelse på treemap
-      .padding(4) //afstand mellem rektanglerne 
-      (root);
+  // Definerer farveskala for eksport
+  var exportColor = d3.scaleLinear()
+    .domain([0, maxValue])
+    .range(["#fdebd0", "#e67e22"]);
 
-    svg.selectAll("rect").remove();
-    svg.selectAll("text").remove();
+  // Stratificerer data
+  var root = d3.stratify()
+    .id(function(d) { return d.name; })
+    .parentId(function(d) { return d.parent; })
+    (data);
 
-      // Tegn rektangler baseret på data
-    svg.selectAll("rect")
-     // Bind data til rektanglerne
-      .data(root.leaves())
-      .enter()
-      .append("rect")
-       // Indstil x-positionen for hvert rektangel
-      .attr('x', function (d) { return d.x0; })
-        // Indstil y-positionen for hvert rektangel
-      .attr('y', function (d) { return d.y0; })
-      // Indstil bredden af hvert rektangel
-      .attr('width', function (d) { return d.x1 - d.x0; })
-      // Indstil højden af hvert rektangel
-      .attr('height', function (d) { return d.y1 - d.y0; })
-      .style("stroke", "black")
-      .style("fill", function(d) { return colorScale(d.data.LAND); })
-      //hånter mus knap 
-      .on("click", function(event, d) {
-        // Vis tooltip ved klik
+  // Opsummerer værdierne
+  root.sum(function(d) { return d.value; });
+
+  // Beregner treemap layoutet
+  d3.treemap()
+    .size([width, height])
+    .padding(4)(root);
+
+     // Vælger alle rektangel-elementer og binder data til dem
+svg.selectAll("rect")
+    // Binder data (treemap blade) til rektangler
+    .data(root.leaves())
+    // Opretter nye rektangler for hver datapunkt
+    .enter()
+    .append("rect")
+    // Angiver x-positionen for rektanglet
+    .attr('x', function (d) { return d.x0; })
+    // Angiver y-positionen for rektanglet
+    .attr('y', function (d) { return d.y0; })
+    // Angiver bredden på rektanglet
+    .attr('width', function (d) { return d.x1 - d.x0; })
+    // Angiver højden på rektanglet
+    .attr('height', function (d) { return d.y1 - d.y0; })
+    // Tilføjer sort kant til rektanglet
+    .style("stroke", "black")
+    // Tilføjer klik-hændelse for at vise tooltip
+    .on("click", function(event, d) {
+        // Viser tooltip ved klik og opdaterer indholdet
         tooltip.style("opacity", 1)
               .html("<p>Land: " + d.data.LAND + "</p><p>Indhold: " + (d.data.INDHOLD * 1000).toLocaleString('da-DK') + " kr</p>")
               .style("left", (event.pageX + 10) + "px")
               .style("top", (event.pageY + 10) + "px");
-      })
-       .on("mouseout", function() {
-        // Skjul tooltip ved mouseout
+    })
+      //Når musen bevæger sig væk fjernes tekstboksen
+      .on("mouseout", function() {
         tooltip.style("opacity", 0);
-      });
-       // Tilføj tekst til hvert rektangel
-    svg.selectAll("text")
-      .data(root.leaves())
-      .enter()
-      .append("text")
-       // Indstil x-positionen for text
-      .attr("x", function(d){ return d.x0 + 10 })
-        // Indstil y-positionen for text
-      .attr("y", function(d){ return d.y0 + 20 })
-      .text(function(d){ return d.data.LAND })
-      .attr("font-size", "12px")
-      .attr("fill", "white");
+      })
+    .style("fill", function(d) {
+      // Vælger farve afhængigt af type (Import/Eksport)
+      return d.data.type === "Import" ? importColor(d.data.value) : exportColor(d.data.value);
+    });
 
-    svg.selectAll("text.indhold")
-      .data(root.leaves())
-      .enter()
-      .append("text")
-      .attr("x", function(d){ return d.x0 + 10 })
-      .attr("y", function(d){ return d.y0 + 35 })
-       // Tekstindholdet er landets navn i dette tilfælde, som hentes fra dataobjektet
-      .text(function(d){ return 'Indhold: ' + d.data.INDHOLD })
-      .attr("font-size", "12px")
-      .attr("fill", "white");
+  // Tilføjer tekst
+  svg.selectAll("text")
+    .data(root.leaves())
+    .enter()
+    .append("text")
+    .attr("x", function(d){ return d.x0 + 10; })
+    .attr("y", function(d){ return d.y0 + 20; })
+    .text(function(d){ return d.data.name; })
+    .attr("font-size", "12px")
+    .attr("fill", "white");
 
-    svg.selectAll("text.sitc")
+  // Tilføjer SITC/kategori tekst 
+  svg.selectAll(".sitc-text")
+    .data(root.leaves())
+    .enter()
+    .append("text")
+    .attr("x", function(d){ return d.x0 + 10; })
+    .attr("y", function(d){ return d.y0 + 35; })
+    .text(function(d){ return d.data.SITC; })
+    .attr("font-size", "12px")
+    .attr("fill", "white")
+    .attr("class", "sitc-label");
+
+   // Tilføjer indhold tekst 
+   svg.selectAll(".indhold")
       .data(root.leaves())
       .enter()
       .append("text")
       .attr("x", function(d){ return d.x0 + 10 })
       .attr("y", function(d){ return d.y0 + 50 })
-      .text(function(d){ return 'SITC: ' + d.data.SITC })
+      .text(function(d){ return 'Indhold: ' + d.data.INDHOLD })
       .attr("font-size", "12px")
       .attr("fill", "white");
 
-       svg.selectAll("text.tid")
-      .data(root.leaves())
-      .enter()
-      .append("text")
-      .attr("x", function(d){ return d.x0 + 10 })
-      .attr("y", function(d){ return d.y0 + 65 })
-      .text(function(d){ return 'tid: ' + d.data.TID })
-      .attr("font-size", "12px")
-      .attr("fill", "white");
-        
-
-
-
-  }).catch(function(error) {
-    console.error("Fejl ved indlæsning af data:", error);
-  });
-}
-
-updateTreemap(currentDataFile);
-
-// Event listener til knappen, der skifter data
-d3.select("#toggleData").on("click", function() {
- // Skift mellem import og eksport data
-  if (currentDataFile === '/DB/treemap_import.csv') {
-    currentDataFile = '/DB/treemap_export.csv';
-    d3.select("#toggleData").text("Vis Import");
-  } else {
-    currentDataFile = '/DB/treemap_import.csv';
-    d3.select("#toggleData").text("Vis Eksport");
-  }
-
-  updateTreemap(currentDataFile);
+}).catch(function(error) {
+  console.error("Fejl ved indlæsning af data:", error);
 });
